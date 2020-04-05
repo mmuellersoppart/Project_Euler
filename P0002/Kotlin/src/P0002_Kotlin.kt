c7c41aebda36a7381fb69c44ec17bf876a9af9d3
Marlon Mueller-Soppart <mmuellersoppart@gmail.com>
2020-04-05 10:13:27 -0500

    P0002 Kotlin is tail recursive. P0002 notes done.

--- P0002/Kotlin/src/P0002_Kotlin.kt
+++ P0002/Kotlin/src/P0002_Kotlin.kt
@@ -1,27 +1,30 @@
 //Marlon Mueller-Soppart
 //20200405
-//notes: not too proud of this one.
+//notes: not too proud of this one. In fact, I am ashamed. I casually looked up fib tail recursion before attempting
+//it myself.
+//thanks https://www.geeksforgeeks.org/tail-recursion-fibonacci/
+//I will do better going forward.
 
 fun main() {
 
     //fibonacci function
-    fun fibonacci(seqIndex: Int): Int {
-        if(seqIndex == 0) return 1
-        if(seqIndex == 1) return 2
+    fun fibonacci(seqIndex: Int, a: Int, b: Int): Int {
+        if(seqIndex == 0) return a
+        if(seqIndex == 1) return b
 
-        return (fibonacci(seqIndex - 1) + fibonacci(seqIndex - 2))
+        return fibonacci((seqIndex - 1), b, a + b)
     }
 
     //find the highest index
     val max = 4000000
     val lastIndex: Int;
     var currIndex = 0;
-    while (fibonacci(currIndex) < max) {
+    while (fibonacci(currIndex, 1, 2) < max) {
         currIndex++
     }
 
     //get all the evens and sum
     val listOfIndexs = 1..currIndex step 3
-    val listOfFibs = listOfIndexs.map { x -> fibonacci(x) }.filter { x -> (x < max) }
+    val listOfFibs = listOfIndexs.map { x -> fibonacci(x, 1, 2) }.filter { x -> (x < max) }
     println("P0002: ${listOfFibs.sum()}")
 }
